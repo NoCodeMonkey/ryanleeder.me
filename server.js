20c7a58cbb579c39342fa63735b9a8cc40c333c6
Ryan Leeder <ryanleeder@gmail.com>
2018-07-12 18:39:16 +1000

    Updates

--- server.js
+++ server.js
@@ -18,30 +18,7 @@ const nconf = require('./config/index').initialize();
 var port = nconf.get('PORT');
 var app = express();
 app.use(helmet());
-if (nconf.get('NODE_ENV') !== 'production') {
-  app.use(serveStatic(path.join(__dirname, 'public')));
-} else {
-  app.use(serveStatic(path.join(__dirname, 'public'), {
-    maxAge: '1y',
-    setHeaders: function (res, path, stat) {
-      var fileType = mime.getType(path);
-      switch(fileType){
-        case "text/javascript":
-        case "application/javascript":
-        case "text/css":
-          res.setHeader('Cache-Control', 'public, max-age=' + 60 * 60 * 24 * 30);
-          res.setHeader('Expires', Date.now() + 60 * 60 * 24 * 1000 * 30);
-			    break;
-        case "image/png":
-        case "image/jpeg":
-        case "image/svg+xml":
-          res.setHeader('Cache-Control', 'public, max-age=' + 60 * 60 * 24 * 365);
-          res.setHeader('Expires', Date.now() + 60 * 60 * 24 * 1000 * 365);
-			    break;
-      }
-    }
-  }));
-}
+app.use(serveStatic(path.join(__dirname, 'public')));
 app.use(markoExpress()); //enable res.marko(template, data)
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json({limit: '1mb'}));
